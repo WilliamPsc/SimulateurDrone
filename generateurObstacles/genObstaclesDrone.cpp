ffed0121c67925feaa1ae7b9e02254dce630bf74
William Psc <pensec.william@gmail.com>
2020-11-10 20:36:18 +0100

    Changement version de générateur d'obstacles

--- generateurObstacles/genObstaclesDrone.cpp
+++ generateurObstacles/genObstaclesDrone.cpp
@@ -1,126 +1,88 @@
-// genObstaclesDrone.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+/**
+* @date			05/11/2020
+* @version		2.0
+* @author		William PENSEC | M2 LSE
+* @description	Génération d'obstacles aléatoires pour une simulation de drônes. Les obstacles ne doivent pas se superposer.
+*
+**/
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <time.h>
-#include <Windows.h>
-
-#define POSITIONX 1000
-#define POSITIONY 1000
-
-#define ORIENTATIONX 0
-#define ORIENTATIONY 0
-#define ORIENTATIONZ 0
-
-#define SHAPEX 100
-#define SHAPEY 100
-#define SHAPEZ 50
-
+#include <vector>
+#include "rapidxml/rapidxml.hpp"
+#include "rapidxml/rapidxml_iterators.hpp"
+#include "rapidxml/rapidxml_print.hpp"
+#include "rapidxml/rapidxml_utils.hpp"
 
 using namespace std;
-const char* nomFichier = "batiments.xml";
+using namespace rapidxml;
+
+const char* nomIn = "map.osm";
+const char* fichierTemp = "tmp.xml";
+const char* nomOut = "batiments.xml";
 
 int main(int argc, char* argv[], char* envp[])
 {
-	if (argc != 2) {
-		cout << "Usage : " << argv[0] << " <nombre d'obstacles>\n";
+	// Vérifications arguments
+	if (argc != 1) {
+		cout << "Usage : " << argv[0] << endl;
 		exit(0);
 	}
-	else {
-		cout << "Usage : " << argv[0] << " " << argv[1] << "\n";
-	}
 
-	int nbObstacles = atoi(argv[1]);
-	string objet = "\t<object ";
-	string position = "position=\"min ";
-	string orientation = "orientation=\"";
-	string shape = "shape=\"cuboid ";
-	string materiel = "material=\"brick\" ";
-	string color = "fill-color=\"255 100 50\" ";
-	string opacity = "opacity=\"0.9\" ";
-	string texture = "texture=\"brick.jpg\"/> ";
-	string commentaire = "\t<!-- 10m de long / 50m de large / 10m de haut-->";
-	string final = "";
+	// Variables
+	string ligne;
+	int i = 1;
+	ofstream fileTemp;
+	ofstream fileOut;
+	ifstream fileIn;
+	xml_document<> doc;
+	xml_node<>* rootNode;
+	bool premiereValeur = true;
+
+	// Ouverture du fichier de lecture
+	fileIn.open(nomIn, ios::in);
+
+	// Ouverture des fichiers d'écritures
+	fileTemp.open(fichierTemp, ios::out | ios::trunc);
+	/*fileOut.open(nomOut, ios::out | ios::trunc);
+	fileOut << "<environment>" << endl;
+	fileOut << "\t<!-- Obstacles -->" << endl;*/
+
+	// Lire le fichier xml
+	vector<char> buffer((istreambuf_iterator<char>(fileIn)), istreambuf_iterator<char>());
+	buffer.push_back('\0');
 
 
-	cout << "Génération de : " << nbObstacles << " obstacles\n";
+	// Récupérer le fichier en le parsant
+	doc.parse<0>(&buffer[0]);
+	rootNode = doc.first_node("osm");
 
-	ofstream file;
-	file.open(nomFichier, ios::out | ios::trunc);
-	file << "<environment>" << endl;
-	file << "\t<!-- Obstacles -->" << endl;
 
-	// Initialisation du rand()
-	srand(time(0));
-
-	for (int i = 0; i < nbObstacles; i++) {
-		int x, y, z;
-		// position
-		x = rand() % POSITIONX;
-		y = rand() % POSITIONY;
-
-		position += to_string(x) + " ";
-		position += to_string(y);
-		position += " 0\" ";
-
-		// orientation
-		if (ORIENTATIONX != 0 && ORIENTATIONY != 0 && ORIENTATIONZ != 0) {
-			x = rand() % ORIENTATIONX;
-			y = rand() % ORIENTATIONY;
-			z = rand() % ORIENTATIONZ;
+	// Récupérer les informations
+	for (xml_node<>* wayNode = rootNode->first_node("way"); wayNode != 0; wayNode = wayNode->next_sibling()) {
+		for (xml_node<>* tagNode = wayNode->first_node("tag"); tagNode != 0; tagNode = tagNode->next_sibling()) {
+			if (strcmp(tagNode->first_attribute("k")->value(), "building") == 0) {
+				/*for (xml_node<>* ndNode = wayNode->first_node("nd"); ndNode != 0; ndNode = ndNode->next_sibling()) {
+					for (xml_node<>* nodeNode = rootNode->first_node("node"); nodeNode != 0 ; nodeNode = nodeNode->next_sibling()) {
+						if (strcmp(nodeNode->first_attribute("id")->value(), ndNode->first_attribute("ref")->value()) == 0) {
+							fileTemp << "Latitude : " << nodeNode->first_attribute("lat")->value() << " | Longitude : " << nodeNode->first_attribute("lon")->value() << endl;
+						}
+					}
+				}*/
+				fileTemp << wayNode->name() << endl;
+				//fileTemp << endl;
+			}
 		}
-		else {
-			x = 0;
-			y = 0;
-			z = 0;
-		}
-
-		orientation += to_string(x) + " ";
-		orientation += to_string(y) + " ";
-		orientation += to_string(z) + "\" ";
-
-		// shape
-		x = rand() % SHAPEX + 10;
-		y = rand() % SHAPEY + 10;
-		z = rand() % SHAPEZ + 10;
-
-		shape += to_string(x) + " ";
-		shape += to_string(y) + " ";
-		shape += to_string(z) + "\" ";
-
-		// commentaire
-		commentaire = "\t<!-- ";
-		commentaire += to_string(x) + "m de long / ";
-		commentaire += to_string(y) + "m de large / ";
-		commentaire += to_string(z) + "m de haut";
-		commentaire += "-->";
-
-		// assemblage
-		final = objet + position + orientation + shape + materiel + color + opacity + texture + commentaire;
-
-		// écriture dans fichier
-		file << final << endl;
-
-		// réinitialisation des variables
-		objet = "\t<object ";
-		position = "position=\"min ";
-		orientation = "orientation=\"";
-		shape = "shape=\"cuboid ";
-		materiel = "material=\"brick\" ";
-		color = "fill-color=\"255 100 50\" ";
-		opacity = "opacity=\"0.9\" ";
-		texture = "texture=\"brick.jpg\"/> ";
-		commentaire = "\t<!-- 10m de long / 50m de large / 10m de haut-->";
-		final = "";
-		//Sleep(1000);
 	}
 
 
-	//file << "\n" << endl;
-	file << "</environment>" << endl;
+	// Fermer la balise avant de finir le fichier
+	//fileOut << "</environment>" << endl;
 
-	file.close();
+	// Fermeture du fichier et sortie du programme
+	fileIn.close();
+	/*fileOut.close();*/
+	fileTemp.close();
 	return EXIT_SUCCESS;
 }
